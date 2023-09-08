1c4f0a2b1b4852f2a79f91e5e4aa9ccb7c09ee4e
RichardLeyvaP <richardleyvap1991@gmail.com>
2023-09-08 01:52:13 -0300

    pagina Login.
    si entra un tipo de usurio lo mada a una pagina,si entra admin por ejemplo lo manda a otra diferente,dentro de lo realizado esto es lo mas importante el Loguin y el salir en cada pagina

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:turnopro_apk/Controllers/coexistence.controller.dart';
 import 'package:turnopro_apk/Controllers/customer.controller.dart';
+import 'package:turnopro_apk/Controllers/login.controller.dart';
 import 'package:turnopro_apk/Controllers/main.controller.dart';
 import 'package:turnopro_apk/Controllers/notification.controller.dart';
 import 'package:turnopro_apk/Controllers/service.controller.dart';
 import 'package:turnopro_apk/Routes/index.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:turnopro_apk/Views/homeResponsiblePage.dart';
 
 void main() {
   // Llamada para inicializar Flutter y su enlace con la plataforma
@@ -24,61 +26,94 @@ void main() {
 // ignore: must_be_immutable
 class Myapp extends StatelessWidget {
   Myapp({super.key});
+  final LoginController controller = Get.put(LoginController());
   Color colorPrimario =
       const Color.fromARGB(255, 241, 130, 84); // Color primario appBar
   Color colorSecundario =
       const Color.fromARGB(155, 231, 232, 234); // Color secundario
+  Color colorPrimario2 =
+      const Color.fromARGB(255, 26, 50, 82); // Color primario appBar
+  Color colorSecundario2 =
+      const Color.fromARGB(155, 36, 86, 185); // Color secundario
 
   @override
   Widget build(Object context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          toolbarHeight: 120, // Cambia este valor según tus necesidades
-          backgroundColor: colorPrimario,
+    return GetBuilder<LoginController>(builder: (_) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: _.pagina == '/home'
+            ? ThemeData(
+                appBarTheme: AppBarTheme(
+                  toolbarHeight: 120, // Cambia este valor según tus necesidades
+                  backgroundColor: colorPrimario,
+                ),
+                primaryColor: colorPrimario, // Color primario
+                hintColor: colorSecundario,
+                textTheme: GoogleFonts
+                    .poppinsTextTheme(), // Aplicar Poppins a todo el proyecto
+                // Otras configuraciones de tema
+              )
+            : ThemeData(
+                appBarTheme: AppBarTheme(
+                  toolbarHeight: 120, // Cambia este valor según tus necesidades
+                  backgroundColor: colorPrimario2,
+                ),
+                primaryColor: colorPrimario2, // Color primario
+                hintColor: colorSecundario2,
+                textTheme: GoogleFonts
+                    .poppinsTextTheme(), // Aplicar Poppins a todo el proyecto
+                // Otras configuraciones de tema
+              ),
+        initialRoute: '/login',
+        unknownRoute: GetPage(
+          name: '/Error', // Nombre de la ruta de error
+          page: () => const Page404(), // Página de error
         ),
-        primaryColor: colorPrimario, // Color primario
-        hintColor: colorSecundario,
-        textTheme: GoogleFonts
-            .poppinsTextTheme(), // Aplicar Poppins a todo el proyecto
-        // Otras configuraciones de tema
-      ),
-      initialRoute: '/home',
-      unknownRoute: GetPage(
-        name: '/Error', // Nombre de la ruta de error
-        page: () => const Page404(), // Página de error
-      ),
-      getPages: [
-        GetPage(name: '/home', page: () => const HomePages()),
-        GetPage(
-          name:
-              '/RapiUsers', //*ESTA RUTA ESTA MAL A PROPOSITO LA RUTA REAL ES /apiUsers
-          page: () => const UserPage(),
-          binding: BindingsBuilder.put(() => MainController()),
-        ), //ESTA ESTA CARGANDO UNA API
-        GetPage(
-          name: '/servicesProductsPage',
-          page: () => const ServicesProductsPage(),
-          binding: BindingsBuilder.put(() => ServiceController()),
-        ), //ESTA ESTA CARGANDO UNA API
-        GetPage(
-          name: '/NotificationsPageNew',
-          page: () => const NotificationsPageNew(),
-          binding: BindingsBuilder.put(() => NotificationController()),
-        ),
-        GetPage(
-          name: '/clients',
-          page: () => const CustomersPage(),
-          binding: BindingsBuilder.put(() => CustomerController()),
-        ), //ESTA ESTA CARGANDO UNA API
-        GetPage(
-          name: '/CoexistencePage',
-          page: () => const CoexistencePage(),
-          binding: BindingsBuilder.put(() => CoexistenceController()),
-        ),
-        GetPage(name: '/Error', page: () => const Page404()),
-      ],
-    );
+        getPages: [
+          GetPage(
+            name: '/home',
+            page: () => const HomePages(),
+            binding: BindingsBuilder.put(() => LoginController()),
+          ),
+          GetPage(
+            name: '/HomeResponsible',
+            page: () => const HomeResponsiblePages(),
+            binding: BindingsBuilder.put(() => LoginController()),
+          ),
+          GetPage(
+            name: '/login',
+            page: () => LoginPage(),
+            binding: BindingsBuilder.put(() => LoginController()),
+          ),
+          GetPage(
+            name:
+                '/RapiUsers', //*ESTA RUTA ESTA MAL A PROPOSITO LA RUTA REAL ES /apiUsers
+            page: () => const UserPage(),
+            binding: BindingsBuilder.put(() => MainController()),
+          ), //ESTA ESTA CARGANDO UNA API
+          GetPage(
+            name: '/servicesProductsPage',
+            page: () => const ServicesProductsPage(),
+            binding: BindingsBuilder.put(() => ServiceController()),
+          ), //ESTA ESTA CARGANDO UNA API
+          GetPage(
+            name: '/NotificationsPageNew',
+            page: () => const NotificationsPageNew(),
+            binding: BindingsBuilder.put(() => NotificationController()),
+          ),
+          GetPage(
+            name: '/clients',
+            page: () => const CustomersPage(),
+            binding: BindingsBuilder.put(() => CustomerController()),
+          ), //ESTA ESTA CARGANDO UNA API
+          GetPage(
+            name: '/CoexistencePage',
+            page: () => const CoexistencePage(),
+            binding: BindingsBuilder.put(() => CoexistenceController()),
+          ),
+          GetPage(name: '/Error', page: () => const Page404()),
+        ],
+      );
+    });
   }
 }
