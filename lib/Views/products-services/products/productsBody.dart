d2ab62303c1118bf104137b15d08c1c969d90b70
RichardLeyvaP <richardleyvap1991@gmail.com>
2023-09-14 02:07:40 -0300

    Pagina de Productos ya esta lista,
    solo falta cargar de la base de la api la imagen, lo demas carga y en la parte del diseño ya todo esta tomando el alto y el ancho en %.

--- lib/Views/products-services/products/productsBody.dart
+++ lib/Views/products-services/products/productsBody.dart
@@ -47,9 +47,6 @@ class _ProductsBodyState extends State<ProductsBody>
                   Text(tabValues[2]),
                 ]),
           ),
-          SizedBox(
-            height: (MediaQuery.of(context).size.width * 0.005),
-          ),
           Expanded(
               child: TabBarView(
             controller: _tabController,
@@ -104,106 +101,109 @@ class _ProductsBodyState extends State<ProductsBody>
   }
 }
 
-Row newMethod(String addressProduct, String productName, String propertiesName,
-    String priceProduct, BuildContext context) {
+Padding newMethod(String addressProduct, String productName,
+    String propertiesName, String priceProduct, BuildContext context) {
   //VARIABLES DE PROPIEDADES DEL WIDGET
   const double borderRadiusValue = 12; //container que carga la imagen
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-      Container(
-        height: (MediaQuery.of(context).size.height * 0.26),
-        width: (MediaQuery.of(context).size.width * 0.4),
-        decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(borderRadiusValue)),
-            color: Color.fromARGB(255, 241, 130, 84)),
-        child: FractionallySizedBox(
-          widthFactor: 0.6, // 50% del ancho del contenedor padre
-          heightFactor: 0.65, // 50% del alto del contenedor padre
-          child: Center(
-            child: Image.asset(addressProduct),
+  return Padding(
+    padding: const EdgeInsets.only(left: 10, top: 5, right: 10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: (MediaQuery.of(context).size.height * 0.26),
+          width: (MediaQuery.of(context).size.width * 0.4),
+          decoration: const BoxDecoration(
+              borderRadius:
+                  BorderRadius.all(Radius.circular(borderRadiusValue)),
+              color: Color.fromARGB(255, 241, 130, 84)),
+          child: FractionallySizedBox(
+            widthFactor: 0.6, // 50% del ancho del contenedor padre
+            heightFactor: 0.65, // 50% del alto del contenedor padre
+            child: Center(
+              child: Image.asset(addressProduct),
+            ),
           ),
         ),
-      ),
-      const SizedBox(
-        width: 10,
-      ),
-      SizedBox(
-        height: (MediaQuery.of(context).size.height * 0.26),
-        width: (MediaQuery.of(context).size.width * 0.4),
-        child: Column(
-          //AQUI ES LA PARTE DERECHA DE LOS DATOS DEL PRODUCTO
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                productName,
-                style: TextStyle(
-                    fontSize: (MediaQuery.of(context).size.width * 0.06),
-                    fontWeight: FontWeight.w900),
+        SizedBox(
+          height: (MediaQuery.of(context).size.height * 0.26),
+          width: (MediaQuery.of(context).size.width * 0.52),
+          child: Column(
+            //AQUI ES LA PARTE DERECHA DE LOS DATOS DEL PRODUCTO
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ListTile(
+                title: Text(
+                  productName,
+                  style: TextStyle(
+                      fontSize: (MediaQuery.of(context).size.width * 0.05),
+                      fontWeight: FontWeight.w900),
+                ),
+                subtitle: Text(
+                  propertiesName,
+                  style: TextStyle(
+                      fontSize: (MediaQuery.of(context).size.width * 0.03)),
+                ),
               ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                propertiesName,
-                style: TextStyle(
-                    fontSize: (MediaQuery.of(context).size.width * 0.03)),
-              ),
-            ),
-            SizedBox(
-              height: (MediaQuery.of(context).size.width * 0.09),
-            ),
-            Text(
-              priceProduct,
-              style: TextStyle(
-                  fontSize: (MediaQuery.of(context).size.width * 0.08),
-                  fontWeight: FontWeight.w900),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: (MediaQuery.of(context).size.height * 0.035),
-                  width: (MediaQuery.of(context).size.width * 0.2),
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(95, 46, 20, 20),
-                    borderRadius:
-                        BorderRadius.all(Radius.circular(borderRadiusValue)),
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 238, 234, 234),
-                        Color.fromARGB(255, 134, 134, 136),
-                      ],
-                      stops: [0.0, 0.8],
-                      begin: FractionalOffset.centerRight,
-                      end: FractionalOffset.centerLeft,
-                    ),
-                  ),
-                  child: Center(
-                      child: Text(
-                    'Agregar',
+              ListTile(
+                title: Center(
+                  child: Text(
+                    priceProduct,
                     style: TextStyle(
-                        fontSize: (MediaQuery.of(context).size.width * 0.03),
-                        fontWeight: FontWeight.w600),
-                  )),
-                ),
-                CircleAvatar(
-                  radius: (MediaQuery.of(context).size.width *
-                      0.04), // Ajusta este valor según tu preferencia
-                  backgroundColor: const Color.fromARGB(255, 241, 130, 84),
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.white,
-                    size: (MediaQuery.of(context).size.height * 0.03),
+                        fontSize: (MediaQuery.of(context).size.width * 0.06),
+                        fontWeight: FontWeight.w900),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
-      )
-    ],
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: (MediaQuery.of(context).size.height * 0.035),
+                        width: (MediaQuery.of(context).size.width * 0.2),
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(95, 46, 20, 20),
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(borderRadiusValue)),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 238, 234, 234),
+                              Color.fromARGB(255, 134, 134, 136),
+                            ],
+                            stops: [0.0, 0.8],
+                            begin: FractionalOffset.centerRight,
+                            end: FractionalOffset.centerLeft,
+                          ),
+                        ),
+                        child: Center(
+                            child: Text(
+                          'Agregar',
+                          style: TextStyle(
+                              fontSize:
+                                  (MediaQuery.of(context).size.width * 0.03),
+                              fontWeight: FontWeight.w600),
+                        )),
+                      ),
+                      CircleAvatar(
+                        radius: (MediaQuery.of(context).size.width *
+                            0.04), // Ajusta este valor según tu preferencia
+                        backgroundColor:
+                            const Color.fromARGB(255, 241, 130, 84),
+                        child: Icon(
+                          Icons.check,
+                          color: Colors.white,
+                          size: (MediaQuery.of(context).size.height * 0.03),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
   );
 }
