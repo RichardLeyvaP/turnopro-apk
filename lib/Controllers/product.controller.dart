9cc7800c56d16a9b992128efdefba6eb4a156d08
RichardLeyvaP <richardleyvap1991@gmail.com>
2023-10-27 16:30:46 -0300

    actualizacion de otros cambios

--- lib/Controllers/product.controller.dart
+++ lib/Controllers/product.controller.dart
@@ -48,6 +48,7 @@ class ProductController extends GetxController {
   }
 
   Future<void> _fetchcategoryList() async {
+    //todo REVISAR
     // Obtén la lista de categorías
     try {
       category = await repository.getCategoryList();
@@ -78,45 +79,5 @@ class ProductController extends GetxController {
 
   getCategoryList() {
     return category;
-  }
-
-  //Future<List<UserModel>> userList() async => await repository.getUserList();
-
-//***************************************************************
-//*METODOS ELIMINAR
-
-  //*ELIMINAR 1 ELEMENTO
-  void deleteproduct(int index) {
-    if (index >= 0 && index < product.length) {
-      //HACER LLAMADA A REPOSITORYY MANDAR A ELIMINAR EN LA BD
-      //SI SE ELIMINA CORRECTAMENTE MADO HACER LO DE ABAJO,Q ES ELIMINAR EN LA PARTE VISUAL
-
-      if (selectproduct.contains(product[index])) {
-        selectproduct.removeWhere((products) => products == product[index]);
-      }
-    }
-    product.removeAt(index);
-    productListLength = product.length;
-
-    update();
-  }
-
-  //*ELIMINAR  ELEMENTOS SELECCIONADOS
-  void deleteMultipleproduct() {
-    if (selectproduct.isNotEmpty) {
-      //HACER LLAMADA A REPOSITORYY MANDAR A ELIMINAR EN LA BD
-      //SI SE ELIMINA CORRECTAMENTE MADO HACER LO DE ABAJO,Q ES ELIMINAR EN LA PARTE VISUAL
-      product.removeWhere((element) => selectproduct.contains(element));
-      productListLength = product.length;
-      selectproduct = [];
-      update();
-    }
-  }
-
-  void deleteAll() {
-    product = [];
-    selectproduct = [];
-    productListLength = product.length;
-    update();
   }
 }
