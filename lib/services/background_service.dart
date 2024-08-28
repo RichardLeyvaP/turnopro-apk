464e920de7e341e43c2509c7c8f062c2f18f3bac
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-08-28 00:31:39 -0300

    cambio de verion de Simplifies-v1.0.1 a Simplifies V1.0.1

--- lib/services/background_service.dart
+++ lib/services/background_service.dart
@@ -65,7 +65,7 @@ Future<void> initializeService() async {
       isForegroundMode: true,
       // isForegroundMode: true,
       notificationChannelId: notificationChannelIdSilent,
-      initialNotificationTitle: 'Simplifies-v1.0.1',
+      initialNotificationTitle: 'Simplifies V1.0.1',
       initialNotificationContent: '',
     ),
     iosConfiguration: IosConfiguration(
